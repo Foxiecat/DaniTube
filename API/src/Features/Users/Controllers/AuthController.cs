2814ae54babb15812c5b249f1a204bf46a34a2bc
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-08-15 10:49:46 +0200

    Created/begun on controllers

--- API/src/Features/Users/Controllers/AuthController.cs
+++ API/src/Features/Users/Controllers/AuthController.cs
@@ -1,42 +1,46 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using src.Features.Users.DTOs;
 using src.Features.Users.Entities;
 using src.Features.Users.Interfaces;
-using src.Features.Users.Models;
 using src.Services.Interfaces;
 
 namespace src.Features.Users.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AuthController(ITokenService tokenService) : ControllerBase
+public class AuthController(
+    IUserService userService,
+    ITokenService tokenService, 
+    ILogger<UserController> logger) : ControllerBase
 {
     public static User user = new();
      
     [HttpPost("register")]
-    public ActionResult<User> Register(UserDto request)
+    public async Task<ActionResult<UserResponse>> Register(UserRequest request)
     {
-        string hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+        UserResponse? response = await userService.CreateAsync(request);
         
-        user.Id = Guid.NewGuid();
-        user.Username = request.Username;
-        user.Roles = new List<IRole> { new Role {Id = Guid.NewGuid(), Name = "User"} };
-        
-        user.HashedPassword = hashedPassword;
-
-        return Ok(user);
+        return response is null
+            ? BadRequest("Unable to create user")
+            : Ok(response);
     }
 
     [HttpPost("login")]
-    public ActionResult<string> Login(UserDto request)
+    public Task<IActionResult> Login(string? usernameOrEmail, string password)
     {
-        if (request.Username != user.Username || new PasswordHasher<User>()
-                .VerifyHashedPassword(user, user.HashedPassword, request.Password) != PasswordVerificationResult.Success)
-        {
-            return BadRequest("Username or password is incorrect");
-        }
-        
-        string token = tokenService.CreateTokenAsync(user);
-        return Ok(token);
+        throw new NotImplementedException();
     }
+    
+    // public Task<ActionResult<string>> Login(UserResponse request)
+    // {
+    //     if (request.Username != user.Username || new PasswordHasher<User>()
+    //             .VerifyHashedPassword(user, user.HashedPassword, request.Password) == PasswordVerificationResult.Failed)
+    //     {
+    //         return BadRequest("Username or password is incorrect");
+    //     }
+    //     
+    //     string token = tokenService.CreateTokenAsync(user);
+    //     return Ok(token);
+    // }
 }
--- API/src/Features/Users/UserController.cs
+++ API/src/Features/Users/UserController.cs
@@ -1,10 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using src.Features.Users.DTOs;
+using src.Features.Users.Entities;
+using src.Features.Users.Interfaces;
 using src.Services.Interfaces;
 
 namespace src.Features.Users;
 
 [Route("[controller]")]
-public class UserController(ITokenService tokenService, ILogger<UserController> logger) : ControllerBase
+[ApiController]
+public class UserController(
+    IUserService userService,
+    ITokenService tokenService, 
+    ILogger<UserController> logger) : ControllerBase
 {
+    
     
 }
