fcfdb3ecda532b4c8bde85b2580042d2ee083715
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-08-15 10:49:03 +0200

    Created UserRepository.cs file

--- API/src/Features/Users/UserRepository.cs
+++ API/src/Features/Users/UserRepository.cs
@@ -1,6 +1,33 @@
+using System.Linq.Expressions;
+using src.Features.Users.Entities;
+using src.Features.Users.Interfaces;
+
 namespace src.Features.Users;
 
-public class UserRepository
+public class UserRepository : IUserRepository
 {
-    
+    public Task<User> AddAsync(User model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> FindAsync(Expression<Func<User, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> UpdateAsync(User model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(User model)
+    {
+        throw new NotImplementedException();
+    }
 }
