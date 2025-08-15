050d880eb05acc73c6d42672b7640d46af887e7d
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-08-15 10:41:02 +0200

    Deleted unnecessary UserDto.cs

--- API/src/Features/Users/Models/UserDto.cs
+++
@@ -1,8 +0,0 @@
-namespace src.Features.Users.Models;
-
-public class UserDto
-{
-    public Guid Id { get; init; }
-    public string Username { get; init; } = string.Empty;
-    public string Password { get; init; } =  string.Empty;
-}
